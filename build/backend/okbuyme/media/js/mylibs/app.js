7ea3d849201da02ffe5bcaac67d3c6f56f17a0e0
Dieter Limeback <dlimeb@gmail.com>
2011-01-22 16:15:28 -0500

    Fixed typo, removed sample code.

--- build/backend/okbuyme/media/js/mylibs/app.js
+++ build/backend/okbuyme/media/js/mylibs/app.js
@@ -8,8 +8,5 @@
 
 
 $(function(){
-for (var i = 0, max = fakeData.items.length; i < max; i++) {
-  window.items.add(fakeData.items[i]);
-}
-window.app = new AppView;
+  window.app = new AppView;
 });
