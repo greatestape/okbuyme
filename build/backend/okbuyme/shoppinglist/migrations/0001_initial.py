6c4152dbbb314cfc8c69623ceafa517fd692a79b
Sam Bull <sbull@trapeze.com>
2011-01-22 14:11:04 -0500

    Added Item model.
    Homepage now lists all items in db
    Homepage now uses shoppinglist/item_list.html template

---
+++ build/backend/okbuyme/shoppinglist/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Item'
+        db.create_table('shoppinglist_item', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+        ))
+        db.send_create_signal('shoppinglist', ['Item'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Item'
+        db.delete_table('shoppinglist_item')
+
+
+    models = {
+        'shoppinglist.item': {
+            'Meta': {'object_name': 'Item'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['shoppinglist']
