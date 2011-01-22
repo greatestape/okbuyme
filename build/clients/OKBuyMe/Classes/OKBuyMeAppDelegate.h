b4fc00553c987b38a3ca3a58871d0164a7112916
Taylan Pince <taylanpince@gmail.com>
2011-01-22 12:34:28 -0500

    Added skeleton iphone project with basic core data implementation

---
+++ build/clients/OKBuyMe/Classes/OKBuyMeAppDelegate.h
@@ -0,0 +1,31 @@
+//
+//  OKBuyMeAppDelegate.h
+//  OKBuyMe
+//
+//  Created by Taylan Pince on 11-01-22.
+//  Copyright 2011 Hippo Foundry. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+
+@interface OKBuyMeAppDelegate : NSObject <UIApplicationDelegate> {
+@public
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *_managedObjectContext;
+    NSManagedObjectModel *_managedObjectModel;
+    NSPersistentStoreCoordinator *_persistentStoreCoordinator;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
