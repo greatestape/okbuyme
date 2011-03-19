d344607b0fc6a718a20e33d792f0cce9a776be87
Sam Bull <sbull@trapeze.com>
2011-03-19 00:08:39 -0400

    Removed unused egg-based template loader from settings.

--- build/backend/okbuyme/settings.py
+++ build/backend/okbuyme/settings.py
@@ -43,7 +43,6 @@ SECRET_KEY = '4tu#j@!$*w7yfuv$1*wg5n#tt%ow=(ah$@s^x!bofyosr2thpv'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
