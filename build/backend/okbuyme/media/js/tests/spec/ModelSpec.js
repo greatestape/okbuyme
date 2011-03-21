ff1192abe2ba8a5f1c8b2ea84004732182b01604
Dieter Limeback <dlimeb@gmail.com>
2011-03-21 17:41:33 -0400

    Added js tests for want model and collection.

--- build/backend/okbuyme/media/js/tests/spec/ModelSpec.js
+++ build/backend/okbuyme/media/js/tests/spec/ModelSpec.js
@@ -1,15 +1,61 @@
 describe("Want model", function() {
 
-  var want;
+  beforeEach(function(){
+    this.server = sinon.fakeServer.create();
+    this.want = new Want({ name: "want1", resource_uri: "/wants/1" });
+  });
+
+  afterEach(function() {
+    this.server.restore();
+  });
+
+  it("should expose an attribute", function(){
+    expect(this.want.get("name")).toEqual("want1");
+  });
+
+  it("should be able to be deleted", function(){
+    // set how the fake server should respond to a DELETE request
+    this.server.respondWith("DELETE", "/wants/1", [204, {}, '']);
+
+    // set a fake callback method to spy on
+    var destroyCallback = sinon.spy();
+    this.want.bind('destroy', destroyCallback); // "destroy" event fired in success callback of Model.destroy()
+
+    // issue the delete
+    this.want.clear();
+
+    // tell the fake server to respond
+    this.server.respond();
+
+    expect(destroyCallback.called).toBeTruthy();
+  });
+});
+
+
+describe("WantList collection", function() {
 
   beforeEach(function(){
-    want = new Want({
-      name: "want1"
-    });
+    this.server = sinon.fakeServer.create();
+
+    this.want1 = new Want({ name: "want1", resource_uri: "/wants/1" });
+    this.want2 = new Want({ name: "want2", resource_uri: "/wants/2" });
+    this.want3 = new Want({ name: "want3", resource_uri: "/wants/3" });
+    this.wants = new WantList([this.want1, this.want2, this.want3]);
   });
 
-  it("should extend the Backbone native model correctly", function(){
-    expect(want.get("name")).toEqual("want1");
+  afterEach(function(){
+    this.server.restore();
   });
 
+  it("should contain the Wants it was created with", function(){
+    expect(this.wants.at(1).get("name")).toEqual("want2");
+  });
+
+  it("should not contain a model after the model is deleted", function(){
+    this.server.respondWith("DELETE", "/wants/2", [204, {}, '']);
+    this.want2.clear();
+    this.server.respond();
+    expect(this.wants.length).toEqual(2);
+    expect(this.wants).not.toContain(this.want2);
+  });
 });
