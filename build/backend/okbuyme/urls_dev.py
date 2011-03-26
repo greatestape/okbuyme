e1a2cd0797cd97b65cc84a8a280e81dbfbcce7bd
Dieter Limeback <dlimeb@gmail.com>
2011-03-25 20:52:45 -0400

    Added preliminary js view tests and related infrastructure.

--- build/backend/okbuyme/urls_dev.py
+++ build/backend/okbuyme/urls_dev.py
@@ -1,12 +1,18 @@
+import os
+
 from django.conf import settings
 from django.conf.urls.defaults import *
 
 from urls import urlpatterns
+
 
 urlpatterns += patterns('',
     (r'^media/(.*)$', 'django.views.static.serve', {'document_root':
         settings.MEDIA_ROOT}),
     (r'^js-tests/$', 'django.views.generic.simple.direct_to_template', {
         'template': 'javascript/SpecRunner.html'
-    })
+    }),
+    (r'^js-tests/fixtures/(.*)$', 'django.views.static.serve', {
+        'document_root': os.path.join(settings.MEDIA_ROOT, 'js/tests/fixtures/')}
+    ),
 )
