0fe47f65daa85518effa14a32f57cdde2bab9f81
Sam Bull <sam@pocketuniverse.ca>
2011-04-08 22:56:25 -0400

    Created separate, semi-temporary non-auth home page.

--- build/backend/okbuyme/shoppinglist/views.py
+++ build/backend/okbuyme/shoppinglist/views.py
@@ -6,15 +6,17 @@ from shoppinglist.forms import WantForm
 
 
 def list(request):
-    form = WantForm()
     if request.user.is_authenticated():
+        form = WantForm()
         wants = Want.objects.filter(owner=request.user)
+        return simple.direct_to_template(
+                request,
+                'shoppinglist/want_list.html',
+                extra_context={
+                    'want_list': wants,
+                    'form': form},
+                )
     else:
-        wants = Want.objects.none()
-    return simple.direct_to_template(
-            request,
-            'shoppinglist/want_list.html',
-            extra_context={
-                'want_list': wants,
-                'form': form},
-            )
+        return simple.direct_to_template(
+                request,
+                'home.html')
